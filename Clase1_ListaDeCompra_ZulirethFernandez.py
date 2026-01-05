486f0f501ee2e7cc984a7477f7eab390ef235b34
arlefernandez <zfernandez@baufest.com>
2026-01-05 12:51:45 -0300

    primer envio

---
+++ Clase1_ListaDeCompra_ZulirethFernandez.py
@@ -0,0 +1,26 @@
+# ========================================================================================================================================================================
+# CURSO: Introducción a la Programación con Python
+# ALUMNA: Zulireth Fernandez
+# CLASE: 01 - Conceptos Básicos
+# EJERCICIO: Lista de compras
+# Usando la instrucción print() y lo visto en la clase, escribir un programa que muestre en la terminal la lista de productos que necesitamos comprar en el supermercado.
+# Tips:
+# • Podés usar tabuladores para ordenar la # lista.
+# • Recordá que si necesitas una línea en blanco, puedes usar “print()”.
+# ========================================================================================================================================================================
+
+import os 
+os.system ("cls")  # Limpia la pantalla en Windows; usa "clear" en Linux/Mac
+
+print("---------Bienvenido a su lista de supermercado---------")
+print()
+print("Producto         \t Cantidad \t Cate")
+print("Aceite           \t 22        \t Aceites y grasas")
+print("Harina 000       \t 55        \t Harinas") 
+print("Maple de huevo   \t 90        \t Huevos")
+print("Pure de Tomates  \t 120        \t Conservas")
+print("Fideo Codito     \t 34        \t Pastas")
+print("Cajas de leche   \t 22        \t Lácteos\nPan lactal      \t 15        \t Panadería")
+print("Carne molida    \t 8kg       \t Carnes")
+print()
+print("¡Espero que hayas disfrutado la lista!")
